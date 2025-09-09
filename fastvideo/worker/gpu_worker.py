b07f303078e749eb8baefd1eef7be1190f88b4b3
Xingyu Long <xingyulong97@gmail.com>
2025-09-08 22:05:56 -0700

    fixup

--- fastvideo/worker/gpu_worker.py
+++ fastvideo/worker/gpu_worker.py
@@ -946,12 +946,14 @@ class WorkerWrapperBase:
     def update_environment_variables(self, envs_list: list[dict[str,
                                                                 str]]) -> None:
         envs = envs_list[self.rpc_rank]
+        logger.info(f"xxx-before-envs={envs}")
         key = 'CUDA_VISIBLE_DEVICES'
         if key in envs and key in os.environ:
             # overwriting CUDA_VISIBLE_DEVICES is desired behavior
             # suppress the warning in `update_environment_variables`
             del os.environ[key]
         update_environment_variables(envs)
+        logger.info(f"xxx-after-envs={envs}")
 
     def init_worker(self, all_kwargs: list[dict[str, Any]]) -> None:
         # TODO(xingyu): move this to RayWorkerWrapper
